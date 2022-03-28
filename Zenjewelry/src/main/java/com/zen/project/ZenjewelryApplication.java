c7fde167f0b5705cae5b76b9c3aa2a7696ec3f04
KRJWLee <93320051+KRJWLee@users.noreply.github.com>
2022-03-28 14:40:22 +0900

    project
    first upload

---
+++ Zenjewelry/src/main/java/com/zen/project/ZenjewelryApplication.java
@@ -0,0 +1,13 @@
+package com.zen.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZenjewelryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZenjewelryApplication.class, args);
+	}
+
+}
