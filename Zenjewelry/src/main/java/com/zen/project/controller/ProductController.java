4e36c2d01d31a698d32eb585469b427c9eb1c870
BangDongJin <102498445+BangDongJin@users.noreply.github.com>
2022-03-29 11:11:36 +0900

    project

--- Zenjewelry/src/main/java/com/zen/project/controller/ProductController.java
+++ Zenjewelry/src/main/java/com/zen/project/controller/ProductController.java
@@ -3,10 +3,17 @@ package com.zen.project.controller;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.zen.project.service.ProductService;
 
@@ -35,4 +42,5 @@ public class ProductController {
 		
 		return "main";
 	}
+	
 }
