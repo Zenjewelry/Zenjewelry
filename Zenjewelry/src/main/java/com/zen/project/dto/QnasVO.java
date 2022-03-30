c77d0d98a72e57ccd01a98ed709c51d5ba6f5746
shinheemun <99234425+shinheemun@users.noreply.github.com>
2022-03-30 13:38:08 +0900

    20220330 heemun

---
+++ Zenjewelry/src/main/java/com/zen/project/dto/QnasVO.java
@@ -0,0 +1,24 @@
+package com.zen.project.dto;
+
+import java.sql.Timestamp;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import lombok.Data;
+
+@Data
+public class QnasVO {
+
+		private Integer qseq; 
+		@NotNull (message="제목을 입력하세요")
+		@NotEmpty(message="제목을 입력하세요")
+		private String subject;
+		@NotNull (message="내용을 입력하세요")
+		@NotEmpty(message="내용을 입력하세요")
+		private String content;
+		private String reply;
+		private String id; 
+		private String rep;
+		private Timestamp indate;
+}
