747a1e6b3fc38895e04d4936c35037ca97769e8e
KRJWLee <93320051+KRJWLee@users.noreply.github.com>
2022-04-19 23:21:06 +0900

    dd

--- Zenjewelry/src/main/webapp/WEB-INF/views/sql_update.sql
+++ Zenjewelry/src/main/webapp/WEB-INF/views/sql_update.sql
@@ -17,3 +17,14 @@ update products set bestyn='y' where pseq=70;
 update products set bestyn='y' where pseq=69;
 
 select * from promotions
+select * from boards
+
+alter table boards modify title varchar2(100);
+
+update boards set content='어떤지, 좀더 나은 착장방법이 있을지 댓글부탁드려요 !!' where num = 121;
+
+select * from board_replys
+delete from board_replys where reply_num = 25
+
+select * from product_qna
+update product_qna set rep=1;
